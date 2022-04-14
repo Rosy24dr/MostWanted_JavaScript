8132ba87892cb104f1445c4ed0c67818276876b0
Lakota Rose <itsrosebro@gmail.com>
2022-04-14 13:22:17 -0500

    Updated Display_Person Function to show more attibutes.

--- app.js
+++ app.js
@@ -142,6 +142,15 @@ function displayPeople(people) {
 function displayPerson(person) {
     let personInfo = `First Name: ${person.firstName}\n`;
     personInfo += `Last Name: ${person.lastName}\n`;
+    personInfo += `Gender : ${person.gender}\n`;
+    personInfo += `DOB: ${person.dob}\n`;
+    personInfo += `Height: ${person.height}\n`;
+    personInfo += `Weight: ${person.weight}\n`;
+    personInfo += `EyeColor: ${person.eyeColor}\n`;
+    personInfo += `Occupation: ${person.occupation}\n`;
+    personInfo += `Parents: ${person.parents}\n`;
+    personInfo += `Current Spouse: ${person.currentSpouse}\n`;
+    
     //! TODO #1a: finish getting the rest of the information to display //////////////////////////////////////////
     alert(personInfo);
 }
@@ -207,8 +216,10 @@ function searchByTraits(people) {
     });
     return foundObject;
   }
-  
+
 
   
+
+
 
       

befabd0eb03716023c605724f36e48ccee9d267d
rossebell <Rosy24dr@outlook.com>
2022-04-18 11:53:42 -0400

    Potential multiple code to try

--- app.js
+++ app.js
@@ -247,7 +247,7 @@ function searchByTraits(people) {
     "Do you want to search for a single trait or multiple traits? Please enter single or multiple:"
   );
 
-  if (singleOrMultiple = "single") {
+  if (singleOrMultiple === "single") {
     let whichTrait = prompt("Which trait do you want to search by?");
     let whatValue = prompt("What would you like to check?");
     let displayResults = people.filter(function (el) {
@@ -263,7 +263,25 @@ function searchByTraits(people) {
         })
         .join("\n")
     );
-  }
+  }else if (singleOrMultiple === "multiple"){
+    let whichTrait = prompt("Which trait do you want to search by first?");
+    let whatValue = prompt("What would you like to check?");
+    let whichTrait = prompt("Which trait do you want to search by second?");
+    let whatValue = prompt("What would you like to check?");
+    let displayResults = people.filter(function (el) {
+      if (el[whichTrait] === whatValue) {
+        return true;
+      }
+    });
+
+    alert(
+      displayResults
+        .map(function (el) {
+          return `${el.firstName} ${el.lastName}`;
+        })
+        .join("\n")
+
+    )}
 }
   
 
